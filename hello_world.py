cefeaa1ba05a0189bdca7f5c867bd4478cebf48a
jillesca <jillesca@cisco.com>
2023-08-18 15:21:57 +0100

    refactoring code

--- hello_world.py
+++ hello_world.py
@@ -5,80 +5,88 @@ from requests.auth import HTTPBasicAuth
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-URL_BASE = "https://sandbox-nso-1.cisco.com"
+BASE_URL = "https://sandbox-nso-1.cisco.com"
+# BASE_URL = "http://sandbox-nso-1.cisco.com"
 USERNAME = "developer"
 PASSWORD = "Services4Ever"
 
 # Add the 'data' path between restconf and tailf-ncs...
 # show ncs-state version | display restconf
 # show ncs-state daemon-status | display restconf
-URL_DEAMON_STATUS = "/restconf/data/tailf-ncs-monitoring:ncs-state/daemon-status"
-URL_NSO_VERSION = "/restconf/data/tailf-ncs-monitoring:ncs-state/version"
+API_ENDPOINTS = [
+    "/restconf/data/tailf-ncs-monitoring:ncs-state/daemon-status",
+    "/restconf/data/tailf-ncs-monitoring:ncs-state/version",
+]
 
-urls_to_call = [URL_DEAMON_STATUS, URL_NSO_VERSION]
 
-
-def restconf_session() -> list:
-    responses: list = []
+def create_restconf_session() -> requests.Session:
     session = requests.session()
     session.auth = HTTPBasicAuth(USERNAME, PASSWORD)
     session.headers.update({"Accept": "application/yang-data+xml"})
     session.verify = False
-
-    with session as s:
-        for url in urls_to_call:
-            responses.append(get_restconf(s, url))
-    return responses
+    return session
 
 
-def get_restconf(session: requests.Session, url: str) -> dict:
-    res = session.get(URL_BASE + url)
-    return {"xml_reply": res.text}
+def fetch_data(session: requests.Session, endpoint: str) -> str:
+    try:
+        response = session.get(BASE_URL + endpoint)
+        response.raise_for_status()
+        return response.text
+    except requests.exceptions.HTTPError as err:
+        print(f"Http Error: {err=}")
+        exit()
+    except requests.exceptions.ConnectionError as err:
+        print(f"Error Connecting: {err=}")
+        exit()
+    except requests.exceptions.Timeout as err:
+        print(f"Timeout Error: {err=}")
+        exit()
+    except requests.exceptions.RequestException as err:
+        print(f"An unexpected error happened: {err=}")
+        exit()
 
 
-def parse_xml_to_dict(xml: str) -> dict:
+def classify_results(parsed_data: dict) -> dict:
+    if "daemon-status" in parsed_data:
+        return {"daemon_status": parsed_data["daemon-status"]["#text"]}
+    if "version" in parsed_data:
+        return {"nso_version": parsed_data["version"]["#text"]}
+
+
+def parse_xml(xml: str) -> dict:
     return xmltodict.parse(xml)
 
 
-def parse_responses(responses: list) -> list:
-    result: dict = {}
-    for res in responses:
-        result.update(clasify_results(res["xml_reply"]))
-    return result
+def print_results(results: dict) -> None:
+    daemon_status = results.get("daemon_status", "FAILED_TO_GET_STATUS")
+    nso_version = results.get("nso_version", "FAILED_TO_GET_VERSION")
+    msg = f"\nNSO version: {nso_version}"
 
-
-def clasify_results(xml_reply: str) -> dict:
-    res_dict = parse_xml_to_dict(xml_reply)
-    if "daemon-status" in res_dict:
-        return {"daemon_status": res_dict["daemon-status"]["#text"]}
-    if "version" in res_dict:
-        return {"nso_version": res_dict["version"]["#text"]}
-
-
-def print_results(res: dict) -> None:
-    deamon_status = res["daemon_status"]
-    msg = f'\nNSO version: {res["nso_version"]}'
-
-    if not valid_deamon_status(deamon_status):
-        msg += f"\nNSO not running. Status: {deamon_status}"
+    if not valid_deamon_status(daemon_status):
+        msg += f"\nNSO not running. Status: {daemon_status}"
         print(msg)
         raise ValueError(msg)
 
-    msg += f"\nNSO deamon running. Status: {deamon_status.upper()}"
+    msg += f"\nNSO daemon running. Status: {daemon_status.upper()}"
     print(msg)
 
 
 def valid_deamon_status(status: str) -> bool:
     # https://developer.cisco.com/docs/nso/guides/#!nso-system-management/monitoring-nso
-    if "started" in status.lower():
-        return True
-    return False
+    return "started" in status.lower()
 
 
 def main() -> None:
-    res = restconf_session()
-    results = parse_responses(res)
-    print_results(results)
+    session = create_restconf_session()
+    responses = [fetch_data(session, endpoint) for endpoint in API_ENDPOINTS]
+
+    parsed_responses = [parse_xml(response) for response in responses]
+
+    combined_results: dict = {}
+    for parsed_response in parsed_responses:
+        combined_results.update(classify_results(parsed_response))
+
+    print_results(combined_results)
 
 
 if __name__ == "__main__":
